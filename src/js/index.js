72e5c592146c145df60ca529761126d0705bd163
wkallil <133437916+wkallil@users.noreply.github.com>
2023-07-01 11:23:02 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+// Direcionar para a página dois
+function redirecionarParaPaginaDois() {
+    window.location.href = "src/html/paginaDois.html";
+  }
+  
+  // Direcionar para a página três
+  function redirecionarParaPaginaTres() {
+    window.location.href = "pagina3.html";
+  }
+
+  // Direcionar para a página principal (index.html)
+function redirecionarParaPaginaPrincipal() {
+  window.location.href = "../../index.html";
+}
